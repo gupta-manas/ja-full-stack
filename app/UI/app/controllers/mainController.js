3c701edbede001674ce07560e97c66a17c5b3c8c
gupta-manas <manas.niet@hotmail.com>
2017-04-13 00:03:10 +0530

    Refresh table on modal close

--- app/UI/app/controllers/mainController.js
+++ app/UI/app/controllers/mainController.js
@@ -1,11 +1,4 @@
 app.controller('mainController', function($scope, $uibModal, BeanService) {
-    /*$scope.demoBeans = [
-        { beanID: 1, intNum: 12, stringVal: 'str1', boolVal: true },
-        { beanID: 2, intNum: 22, stringVal: 'str2', boolVal: true },
-        { beanID: 3, intNum: 41, stringVal: 'str3', boolVal: false },
-        { beanID: 4, intNum: 35, stringVal: 'str4', boolVal: false },
-        { beanID: 5, intNum: 96, stringVal: 'str5', boolVal: true }
-    ];*/
 
     $scope.demoBeans = [];
     BeanService.getBeans().then(function(data) {
@@ -22,6 +15,7 @@ app.controller('mainController', function($scope, $uibModal, BeanService) {
 
         modalInstance.result.then(function(data) {
             console.log("Modal Submit clicked!");
+            $scope.demoBeans.push(data);
         }, function() {
             console.log("Modal canceled at " + new Date())
         })
