d22bf2b005f101a7064680af37f3b6cb9f672931
gupta-manas <manas.niet@hotmail.com>
2017-04-13 11:07:33 +0530

    implemented delete. update model on http requests

--- app/UI/app/controllers/mainController.js
+++ app/UI/app/controllers/mainController.js
@@ -1,9 +1,9 @@
 app.controller('mainController', function($scope, $uibModal, BeanService) {
 
-    $scope.demoBeans = [];
-    BeanService.getBeans().then(function(data) {
+    $scope.demoBeans = BeanService.res.tableData;
+    /*BeanService.getBeans().then(function(data) {
         $scope.demoBeans = data;
-    });
+    });*/
 
     $scope.open = function(size) {
         var modalInstance = $uibModal.open({
@@ -15,10 +15,14 @@ app.controller('mainController', function($scope, $uibModal, BeanService) {
 
         modalInstance.result.then(function(data) {
             console.log("Modal Submit clicked!");
-            $scope.demoBeans.push(data);
+            //$scope.demoBeans.push(data);
         }, function() {
             console.log("Modal canceled at " + new Date())
         })
-    }
+    };
+
+    $scope.deleteBean = function(beanID, rowIndex) {
+        BeanService.deleteBean(beanID, rowIndex);
+    };
 
 });
--- app/UI/app/services/BeanService.js
+++ app/UI/app/services/BeanService.js
@@ -1,4 +1,11 @@
 app.service('BeanService', function($http) {
+
+    var tableData = [];
+
+    this.res = {
+        tableData: tableData
+    }
+
     var successHandler = function() {
         console.log("Operation Success!")
     }
@@ -8,12 +15,23 @@ app.service('BeanService', function($http) {
 
     this.getBeans = function() {
         var promise = $http.get("http://localhost:8080/beans").then(function(res) {
+            tableData = res.data;
             return res.data;
         }, faultHandler);
         return promise;
     }
     this.createBean = function(bean) {
         $http.post('http://localhost:8080/beans', bean)
-            .then(successHandler, faultHandler)
+            .then(function(res) {
+                tableData.push(res.data)
+            }, faultHandler);
+    }
+
+    this.deleteBean = function(beanID, rowIndex) {
+        $http.delete('http://localhost:8080/beans/' + beanID)
+            .then(function() {
+                console.log("Bean with beanID " + beanID + " deleted successfully.");
+                tableData.splice(rowIndex, 1);
+            }, faultHandler);
     }
 });
